017592787d89f84a9ca61321e2e438e7a59d8910
maiquelveve <maiquelveve2008@gmail.com>
2020-03-07 18:40:38 -0300

    add o cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 import './database/index';
 
@@ -11,6 +12,7 @@ class App {
     }
 
     middleware() {
+        this.server.use(cors())
         this.server.use(express.json())
     }   
 
