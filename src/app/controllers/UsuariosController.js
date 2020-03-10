051b4199d5a4eaa43420d842a634c245d9808ae8
maiquelveve <maiquelveve2008@gmail.com>
2020-03-10 00:54:57 -0300

    feita a verificação se email ja existe e add try-catch e mudado o statuas 400 quando ocorrer erros

--- src/app/controllers/UsuariosController.js
+++ src/app/controllers/UsuariosController.js
@@ -3,11 +3,23 @@ import Usuario from '../models/Usuario';
 class UsuariosController {
 
     async cadastrar(req, res) {
-        const usuario = await Usuario.create(req.body);
-        const retorno = [{success: 1, msg: 'ok', user: usuario}]
-        //const retorno = [{success: 0, msg:'Email já cadastrado'}]
+        try {
+            //fazer aqui as validações com yup
+
+            const emailExists = await Usuario.findOne({ where: {st_email: req.body.st_email} })
+            if(emailExists) {
+                const retorno = [{success: 0, msg:'Email já cadastrado'}]
+                return res.status(400).json(retorno)
+            } 
         
-        return res.status(200).json(retorno)
+            const usuario = await Usuario.create(req.body);
+            const retorno = [{success: 1, msg: 'ok', user: usuario}]                
+            return res.status(200).json(retorno)    
+
+        } catch (error) {
+            const retorno = [{success: 0, msg: 'Ocorreu um erro. Verifique os dados informados!'}]                
+            return res.status(400).json(retorno)    
+        }
     }
 }
 
