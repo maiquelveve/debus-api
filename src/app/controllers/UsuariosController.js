e7344b8e914b100488d00e76f0fc435ec620c8cb
Maiquel Leites <maiquelveve2008@gmail.com>
2020-03-10 13:22:26 -0300

    add as validações do req.body

--- src/app/controllers/UsuariosController.js
+++ src/app/controllers/UsuariosController.js
@@ -1,10 +1,38 @@
 import Usuario from '../models/Usuario';
+import * as yup from 'yup';
+
+import validacaoDefinicao from '../../config/validacaoDefinicao';
 
 class UsuariosController {
 
     async cadastrar(req, res) {
         try {
-            //fazer aqui as validações com yup
+            //Validaçõe do req.body Inicio
+            yup.setLocale(validacaoDefinicao)
+            const schemaValidate = yup.object().shape({
+                st_nome: yup
+                    .string()
+                    .max(50)
+                    .required(),
+                    
+                st_email: yup
+                    .string()
+                    .max(50)
+                    .email()
+                    .required(),
+                    
+                st_senha: yup    
+                    .string()
+                    .max(50)
+                    .required()
+            })
+
+            const erros = await schemaValidate.validate(req.body, { abortEarly: false }).then( () => false ).catch( () => true)
+            if(erros === true) {
+                const retorno = [{success: 0, msg: 'Ocorreu um erro. Verifique os dados informados!'}]
+                return res.status(400).json(retorno)
+            }                            
+            //Validaçõe do req.body FINAL
 
             const emailExists = await Usuario.findOne({ where: {st_email: req.body.st_email} })
             if(emailExists) {
