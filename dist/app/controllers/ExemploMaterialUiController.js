17469c44fcadb1e2022d5720bed514ffa3d170e7
maiquelveve <maiquelveve2008@gmail.com>
2020-05-26 01:07:14 -0300

    removida a pasta dist mas manteve-se os build e start do packege.json

--- dist/app/controllers/ExemploMaterialUiController.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});class ExemploMaterialUiController {
-
-    async exemploMaterialUi(req, res){
-        console.log(req.body)
-        return res.status(200).json({ok:true})
-    }
-}
-
-exports. default = new ExemploMaterialUiController();
--- dist/config/authConfig.js
+++
@@ -1,4 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});exports. default = {
-    secret: '6e550a642f3d5f2d2a364ab96697080f',
-    expiresIn: '7d'
-}
